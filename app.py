a50b0b8d09552592c6195ca44ebfd76d3b229547
meldoubleu <80065863+meldoubleu@users.noreply.github.com>
2024-11-29 13:40:16 +0000

    code from rag workshop

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from rag import qa_bot
+
+def main():
+    st.title("DTC Q&A System")
+
+    with st.form(key='rag_form'):
+        prompt = st.text_input("Enter your prompt")
+        response_placeholder = st.empty()
+        submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button:
+        response_placeholder.markdown("Loading...")
+        response = qa_bot(prompt)
+        response_placeholder.markdown(response)
+
+if __name__ == "__main__":
+    main()
